5d4c153fab1b61ccbca31601fcae0f035f39b302
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2023-02-23 12:51:20 +0400

    Delete frontend folder

--- frontend/src/features/home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { screen } from "@testing-library/react";
-import Home from "./Home";
-import { renderWithProviders } from "utils/testHelpers";
-
-it("should have visit boards", () => {
-  renderWithProviders(<Home />);
-  expect(screen.getByText(/View Boards/i)).toBeVisible();
-});
