975507cd341a78a8653ec4abc7c4d1053e615560
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2023-02-23 12:54:27 +0400

    Delete frontend folder (#2)

--- frontend/src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-import React from "react";
-import ReactDOM from "react-dom";
-import "react-markdown-editor-lite/lib/index.css";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
